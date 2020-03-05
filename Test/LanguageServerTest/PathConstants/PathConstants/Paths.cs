e29c8823498d35e2d0a5913fffa82206e024bad0
Tom852 <thomaskistler@bluewin.ch>
2020-03-05 18:40:12 +0100

    using specific folders per test-area for more structure

--- Test/LanguageServerTest/PathConstants/PathConstants/Paths.cs
+++ Test/LanguageServerTest/PathConstants/PathConstants/Paths.cs
@@ -15,21 +15,21 @@ namespace PathConstants
         public static readonly string langServExe = Path.GetFullPath(Path.Combine(rootPath, "Binaries/DafnyLanguageServer.exe"));
 
         //Compile: Dafny Sourcefiles
-        public static readonly string cp_fineDLL = WithTestPath("compile_as_dll.dfy");
-        public static readonly string cp_fineEXE = WithTestPath("compile_as_exe.dfy");
-        public static readonly string cp_assertion = WithTestPath("compile_assertion_violation.dfy");
-        public static readonly string cp_identifier = WithTestPath("compile_unknown_identifier_error.dfy");
-        public static readonly string cp_postcondition = WithTestPath("compile_postcondition_violation.dfy");
+        public static readonly string cp_fineDLL = WithTestPath("compile/compiles_as_dll.dfy");
+        public static readonly string cp_fineEXE = WithTestPath("compile/compiles_as_exe.dfy");
+        public static readonly string cp_assertion = WithTestPath("compile/assertion_violation.dfy");
+        public static readonly string cp_identifier = WithTestPath("compile/unknown_identifier_error.dfy");
+        public static readonly string cp_postcondition = WithTestPath("compile/postcondition_violation.dfy");
 
         //Compile Outputs
-        public static readonly string cp_out_dll = WithTestPath("compile_as_dll.dll");
-        public static readonly string cp_out_exe = WithTestPath("compile_as_exe.exe");
+        public static readonly string cp_out_dll = WithTestPath("compile/compiles_as_dll.dll");
+        public static readonly string cp_out_exe = WithTestPath("compile/compiles_as_exe.exe");
 
         //Counter Example Dafny Sourcefiles
-        public static readonly string ce_fail1 = WithTestPath("ce_postcondition_violation_1.dfy");
-        public static readonly string ce_fail2 = WithTestPath("ce_postcondition_violation_2.dfy");
-        public static readonly string ce_ok = WithTestPath("ce_postcondition_fullfilled.dfy");
-        public static readonly string ce_2meth = WithTestPath("ce_two_methods.dfy");
+        public static readonly string ce_fail1 = WithTestPath("counterExample/postcondition_violation_1.dfy");
+        public static readonly string ce_fail2 = WithTestPath("counterExample/postcondition_violation_2.dfy");
+        public static readonly string ce_ok = WithTestPath("counterExample/postcondition_fullfilled.dfy");
+        public static readonly string ce_2meth = WithTestPath("counterExample/two_methods.dfy");
 
         //Integration
         public static readonly string int_demofile = WithTestPath("integration_demofile.dfy");
