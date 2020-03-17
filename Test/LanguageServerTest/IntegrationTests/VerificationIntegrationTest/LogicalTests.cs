941b2e10ad411350aed898764d36d485590031ef
Tom852 <thomaskistler@bluewin.ch>
2020-03-17 16:00:40 +0100

    refactored verification tests

--- Test/LanguageServerTest/IntegrationTests/VerificationIntegrationTest/LogicalTests.cs
+++ Test/LanguageServerTest/IntegrationTests/VerificationIntegrationTest/LogicalTests.cs
@@ -1,51 +1,19 @@
-using System;
 using System.Collections.Generic;
-using System.Threading;
 using NUnit.Framework;
-using OmniSharp.Extensions.LanguageServer.Client;
-using TestCommons;
 using Files = TestCommons.Paths;
-
-
 
 
 namespace VerificationIntegrationTest
 {
 
     [TestFixture]
-    public class Tests
+    public class LogicalTests : VerificationBase
     {
-        public IntegrationTestBase m = new IntegrationTestBase("Verification");
-        private List<string> diagnosticList;
-        private readonly System.Threading.EventWaitHandle waitHandle = new System.Threading.AutoResetEvent(false);
-
-        [SetUp]
-        public void Setup()
-        {
-            m.Setup();
-            diagnosticList = default;
-
-            PublishDiagnosticsHandler diagnosticsHandler = (uri, diagList) =>
-            {
-                diagnosticList = diagList.ToStringList();
-                waitHandle.Set();
-            };
-
-            m.Client.TextDocument.OnPublishDiagnostics(diagnosticsHandler);
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            m.TearDown();
-        }
-
 
         [Test]
         public void NoDiagnostics1()
         {
             SendRequestAndAwaitDiagnostics(Files.vc_good);
-
             CollectionAssert.IsEmpty(diagnosticList);
         }
 
@@ -53,9 +21,7 @@ namespace VerificationIntegrationTest
         public void NoDiagnostics2()
         {
             SendRequestAndAwaitDiagnostics(Files.vc_noensure);
-
             CollectionAssert.IsEmpty(diagnosticList);
-
         }
 
 
@@ -69,7 +35,7 @@ namespace VerificationIntegrationTest
                 "R[L6 C15 - L6 C23] - Error - assertion violation - Hint: =="    //Todo dieses hint teil ist ja auch übelst behindert. das == wird ja underlined.
             };
 
-            CollectionAssert.AreEquivalent(expct, diagnosticList);
+            VerifyResults(expct);
         }
 
         [Test]
@@ -83,9 +49,7 @@ namespace VerificationIntegrationTest
                 "R[L3 C0 - L3 C1] - Error - A postcondition might not hold on this return path. - Hint: {"
             };
 
-
-
-            CollectionAssert.AreEquivalent(expct, diagnosticList);
+            VerifyResults(expct);
         }
 
         [Test]
@@ -100,14 +64,10 @@ namespace VerificationIntegrationTest
                 "R[L6 C15 - L6 C23] - Error - assertion violation - Hint: =="
             };
 
-            CollectionAssert.AreEquivalent(expct, diagnosticList);
+            VerifyResults(expct);
         }
 
-        private void SendRequestAndAwaitDiagnostics(string file)
-        {
-            m.Client.TextDocument.DidOpen(file, "dfy");
-            waitHandle.WaitOne();
-        }
+
     }
 
 }
---
+++ Test/LanguageServerTest/IntegrationTests/VerificationIntegrationTest/VerificationBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OmniSharp.Extensions.LanguageServer.Client;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using TestCommons;
+
+namespace VerificationIntegrationTest
+{
+    public class VerificationBase : IntegrationTestBase
+    {
+        protected List<string> diagnosticList;
+        private readonly System.Threading.EventWaitHandle waitHandle = new System.Threading.AutoResetEvent(false);
+
+        public VerificationBase()
+        {
+            name = "Verification";
+        }
+
+        [SetUp]
+        public void CleanResults()
+        {
+            diagnosticList = default;
+        }
+
+        [SetUp]
+        public void RegisterDiagnosticHandler()
+        {
+            PublishDiagnosticsHandler diagnosticsHandler = (uri, diagList) =>
+            {
+                diagnosticList = diagList.ToStringList();
+                waitHandle.Set();
+            };
+
+            Client.TextDocument.OnPublishDiagnostics(diagnosticsHandler);
+        }
+
+
+        protected void SendRequestAndAwaitDiagnostics(string file)
+        {
+            Client.TextDocument.DidOpen(file, "dfy");
+            waitHandle.WaitOne();
+        }
+
+        protected void VerifyResults(List<string> expct)
+        {
+            CollectionAssert.AreEquivalent(expct, diagnosticList);
+        }
+    }
+}
