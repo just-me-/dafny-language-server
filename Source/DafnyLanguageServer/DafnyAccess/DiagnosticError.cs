8a6b3ab0d229d75694a38fc2b0e58ad2b47c2efb
Tom852 <thomaskistler@bluewin.ch>
2020-03-19 17:14:23 +0100

    added own Error Information (Boogie's is protected, Dafny's has not enough Properties) and converters

---
+++ Source/DafnyLanguageServer/DafnyAccess/DiagnosticError.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Boogie;
+using Microsoft.Dafny;
+
+namespace DafnyLanguageServer.DafnyAccess
+{
+    public class DiagnosticError
+    {
+        public IToken Tok { get; set; }
+        public string Msg { get; set; }
+        public List<ErrorInformation.AuxErrorInfo> Aux { get; set; } = new List<ErrorInformation.AuxErrorInfo>();
+
+        public DiagnosticError(IToken tok, string msg)
+        {
+            Tok = tok;
+            Msg = msg;
+        }
+    }
+
+    public static class ConverterExtensions
+    {
+
+        public static DiagnosticError ConvertToErrorInformation(this ErrorMessage eMsg)
+        {
+            return new DiagnosticError(eMsg.token, eMsg.message);
+        }
+
+        public static DiagnosticError ConvertToErrorInformation(this ErrorInformation eInfo)
+        {
+            var result = new DiagnosticError(eInfo.Tok, eInfo.Msg);
+            result.Aux = eInfo.Aux;  //todo evtl clonen wegen refernezen zeug? ne oder?
+            return result;
+        }
+
+        public static List<DiagnosticError> ToDiagnosticErrorList<T>(this IEnumerable<T> source, Func<T, DiagnosticError> converter)
+        {
+            return source?.Select(converter).ToList();
+        }
+
+
+    }
+}
