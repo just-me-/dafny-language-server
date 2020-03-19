36a297355ab7632cc696875d1f852484b9769537
Tom852 <thomaskistler@bluewin.ch>
2020-03-19 18:58:44 +0100

    splittet tests further

--- Test/LanguageServerTest/IntegrationTests/VerificationIntegrationTest/FineExamples.cs
+++ Test/LanguageServerTest/IntegrationTests/VerificationIntegrationTest/FineExamples.cs
@@ -6,7 +6,7 @@ namespace VerificationIntegrationTest
 {
 
     [TestFixture]
-    public class LogicalTests : VerificationBase
+    public class FineExamples : VerificationBase
     {
 
         [Test]
@@ -21,47 +21,6 @@ namespace VerificationIntegrationTest
         {
             SendRequestAndAwaitDiagnostics(Files.vc_noensure);
             CollectionAssert.IsEmpty(diagnosticList);
-        }
-
-
-        [Test]
-        public void AssertionFail()
-        {
-            SendRequestAndAwaitDiagnostics(Files.vc_assertion);
-
-            List<string> expct = new List<string>()
-            {
-                "R[L6 C15 - L6 C23] - Error - Logical Error: assertion violation at [ == ]"
-            };
-
-            VerifyResults(expct);
-        }
-
-        [Test]
-        public void PostConditionFail()
-        {
-            SendRequestAndAwaitDiagnostics(Files.vc_postcondition);
-
-            List<string> expct = new List<string>()
-            {
-                "R[L3 C0 - L3 C1] - Error - Logical Error: A postcondition might not hold on this return path at [ { ] Related Information: R[L2 C16 - L2 C26] This is the postcondition that might not hold."
-            };
-
-            VerifyResults(expct);
-        }
-
-        [Test]
-        public void MultipleFails()
-        {
-            SendRequestAndAwaitDiagnostics(Files.vc_multiplefails);
-
-            List<string> expct = new List<string>()
-            {
-                "R[L3 C0 - L3 C1] - Error - Logical Error: A postcondition might not hold on this return path at [ { ] Related Information: R[L2 C16 - L2 C26] This is the postcondition that might not hold.",
-                "R[L6 C15 - L6 C23] - Error - Logical Error: assertion violation at [ == ]"
-            };
-
-            VerifyResults(expct);
         }
 
 
