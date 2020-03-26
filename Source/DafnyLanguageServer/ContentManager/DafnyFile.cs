e131e65c837828fce2625a9f3ba4969ae38cde96
TomK <tkistler@hsr.ch>
2020-03-26 17:54:32 +0100

    oh wow, that uri conversion caused the tests to fail :cow:

--- Source/DafnyLanguageServer/ContentManager/DafnyFile.cs
+++ Source/DafnyLanguageServer/ContentManager/DafnyFile.cs
@@ -7,7 +7,7 @@ namespace DafnyLanguageServer.ContentManager
     public class DafnyFile
     {
         public Uri Uri { get; set; }
-        public string Filepath => Uri.LocalPath;
+        public string Filepath => Uri.ToString();
         public string Sourcecode { get; set; }
         public FileSymboltable Symboltable { get; set; }
         public IDafnyTranslationUnit DafnyTranslationUnit { get; set; }
