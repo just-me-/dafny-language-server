566d0a60f8ea849ec4bc3e82a98bab4cc876bbc7
TomK <tkistler@hsr.ch>
2020-04-08 15:42:58 +0200

    resolved compile errors

--- Source/DafnyLanguageServer/DafnyAccess/MetaDafnySymboltableBaem.cs
+++ Source/DafnyLanguageServer/DafnyAccess/MetaDafnySymboltableBaem.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Dafny;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using DafnyProgram = Microsoft.Dafny.Program;
 
 namespace DafnyLanguageServer.DafnyAccess
 {
+
+
     public class MetaDafnySymboltableBaem
     {
         public List<SymbolInformation> SymbolTable { get; set; } = new List<SymbolInformation>();
@@ -20,56 +23,58 @@ namespace DafnyLanguageServer.DafnyAccess
         {
             foreach (var module in DafnyProgram.Modules())
             {
-                AddClasses(module, _information);
-                AddMethods(module, _information);
-                AddFields(module, _information);
+                AddClasses(module);
+                //AddMethods(module);
+                //AddFields(module);
             }
         }
-    }
-    private void AddMethods(ModuleDefinition module, List<SymbolInformation> information)
-    {
-    foreach (var clbl in ModuleDefinition.AllCallables(module.TopLevelDecls).Where(e => e != null && !(e.Tok is IncludeToken)))
-    {
 
-    }
-    }
-
-
-    public class SymbolInformation
-    {
-        public Position Position { get; set; }
-        public string Name { get; set;  }
-        
-        //evt wieder weg
-        public ModuleDefinition Module { get; set; }
-
-        public Type Type { get; set; }
-        public SymbolInformation Parent { get; set; } // TopLevelDecl müsste man noch casten 
-        public SymbolInformation DeclarationOrigin { get; set; }
-        public List<SymbolInformation> Children { get; set; }
-        public List<SymbolInformation> Usages { get; set; }
-
-
-        public SymbolInformation()
+        private void AddClasses(ModuleDefinition module)
         {
+            foreach (var clbl in ModuleDefinition.AllCallables(module.TopLevelDecls)
+                .Where(e => e != null && !(e.Tok is IncludeToken)))
+            {
 
+            }
         }
-    }
 
-    public class Position
-    {
-        // col start end
-        // line 
-    }
 
-    public enum Type
-    {
-        Class,
-        Method,
-        Function,
-        Field,
-        Call,
-        Definition,
-        Predicate
+        public class SymbolInformation
+        {
+            public Position Position { get; set; }
+            public string Name { get; set; }
+
+            //evt wieder weg
+            public ModuleDefinition Module { get; set; }
+
+            public Type Type { get; set; }
+            public SymbolInformation Parent { get; set; } // TopLevelDecl müsste man noch casten 
+            public SymbolInformation DeclarationOrigin { get; set; }
+            public List<SymbolInformation> Children { get; set; }
+            public List<SymbolInformation> Usages { get; set; }
+
+
+            public SymbolInformation()
+            {
+
+            }
+        }
+
+        public class Position
+        {
+            // col start end
+            // line 
+        }
+
+        public enum Type
+        {
+            Class,
+            Method,
+            Function,
+            Field,
+            Call,
+            Definition,
+            Predicate
+        }
     }
 }
