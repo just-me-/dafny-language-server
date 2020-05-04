e68433d18fead86ce894a3d9a1f5e7f607af7d53
Just Me <hess-marcel@bluewin.ch>
2020-05-05 01:28:00 +0200

    Basic Version of CodeLense

--- Source/DafnyLanguageServer/Handler/LspStandard/CodeLensTaskHandler.cs
+++ Source/DafnyLanguageServer/Handler/LspStandard/CodeLensTaskHandler.cs
@@ -5,6 +5,7 @@ using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using OmniSharp.Extensions.LanguageServer.Protocol.Server;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using DafnyLanguageServer.DafnyAccess;
@@ -13,6 +14,8 @@ using DafnyLanguageServer.SymbolTable;
 using DafnyServer;
 using Microsoft.Dafny;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace DafnyLanguageServer.Handler
 {
@@ -66,13 +69,67 @@ namespace DafnyLanguageServer.Handler
         {
             Position position = new Position((long)symbolInformation.Line - 1, 0);
             Range range = new Range { Start = position, End = position };
+
+            var locations = new List<Location>();
+            if (symbolInformation.Usages.Any())
+            {
+                foreach (var usage in symbolInformation.Usages)
+                {
+                    var startPos = new Position
+                    {
+                        Line = usage.Position.BodyStartToken.line - 1,
+                        Character = usage.Position.BodyStartToken.col
+                    };
+                    var endPos = new Position
+                    {
+                        Line = usage.Position.BodyEndToken.line - 1,
+                        Character = usage.Position.BodyEndToken.col
+                    };
+                    locations.Add(new Location
+                    {
+                        Uri = request.TextDocument.Uri, // this wont work in references to other files... 
+                        Range = new Range(startPos, endPos)
+                    });
+                }
+            }
+
+            // todo lang file #102
+            // =?: operator
+            string msgTitle;
+            if (symbolInformation.Usages.Any())
+            {
+                if (symbolInformation.Usages.Count == 1)
+                {
+                    msgTitle = $"1 reference to {symbolInformation.Name}";
+                }
+                else
+                {
+                    msgTitle = $"{symbolInformation.Usages.Count} reference(s) to {symbolInformation.Name}";
+                }
+            }
+            else
+            {
+                msgTitle = $"Never used yet. Can you remove {symbolInformation.Name}?";
+            }
+
+            var args = new
+            {
+                Uri = request.TextDocument.Uri,
+                Position = position, // where to show the codelens popup
+                Locations = locations // what should be in the pop up displayed 
+            };
             Command command = new Command
             {
-                Title = (symbolInformation.Usages?.Count) + " reference(s) to " + // todo lang file #102
-                        symbolInformation.Name,
+                Title = msgTitle,
                 Name = "dafny.showReferences",
+                Arguments = new JArray(JsonConvert.SerializeObject(args))
             };
-            return new CodeLens { Data = request.TextDocument.Uri, Range = range, Command = command };
+            return new CodeLens
+            {
+                Data = request.TextDocument.Uri,
+                Range = range,
+                Command = command
+            };
         }
     }
 }
