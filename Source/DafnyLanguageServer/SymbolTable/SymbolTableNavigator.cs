15783a899329f42ee55233b23f290b4a0a97cbbe
TomK <tkistler@hsr.ch>
2020-05-08 15:13:20 +0200

    bracketing error fix, made error at manual merging

--- Source/DafnyLanguageServer/SymbolTable/SymbolTableNavigator.cs
+++ Source/DafnyLanguageServer/SymbolTable/SymbolTableNavigator.cs
@@ -46,7 +46,7 @@ namespace DafnyLanguageServer.SymbolTable
                 }
                 // in case no better match was found,
                 // check default scope too
-                if ((bestMatch == null || bestMatch.Equals(rootEntry) && (child.Name == "_default" || child.Name == "_module")) //in case merge conflict: das module musste ich hier adden weil ich nun auch (neu dazu gekommen) module durchsuchen muss, und halt auch das defualt module wenne ss onst nix gefunden hat. auf ziele 67 ist noch sowas ähnliches, müsste das da cuh hin?
+                if ((bestMatch == null || bestMatch.Equals(rootEntry)) && (child.Name == "_default" || child.Name == "_module")) //in case merge conflict: das module musste ich hier adden weil ich nun auch (neu dazu gekommen) module durchsuchen muss, und halt auch das defualt module wenne ss onst nix gefunden hat. auf ziele 67 ist noch sowas ähnliches, müsste das da cuh hin?
                 {
                     if (child.Children.Any())
                     {
