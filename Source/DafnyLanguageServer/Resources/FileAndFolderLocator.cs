84a9aa595049b711faf901ec48a2e121236b8e63
TomK <tkistler@hsr.ch>
2020-05-20 16:08:49 +0200

    Set up basic file

---
+++ Source/DafnyLanguageServer/Resources/FileAndFolderLocator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DafnyLanguageServer.Resources
+{
+    public class FileAndFolderLocator
+    {
+
+        //Base Path
+        private static readonly string thisAssemblyPath = Path.GetDirectoryName(typeof(FileAndFolderLocator).Assembly.Location);
+
+
+        //Folders
+        public static readonly string rootFolder = Combine(thisAssemblyPath, "../");
+        public static readonly string configPath = CombineWithRoot("Config");
+        public static readonly string testFilesPath = CombineWithRoot("Test/LanguageServerTest/DafnyFiles");
+
+        //Executables
+        public static readonly string dafnyExe = CombineWithRoot("Binaries/Dafny.exe");
+        public static readonly string langServExe = CombineWithRoot("Binaries/DafnyLanguageServer.exe");
+
+
+        
+        
+        private static string Combine(string a, string b) => Path.GetFullPath(Path.Combine(a, b));
+        private static string CombineWithRoot(string b) => Combine(rootFolder, b);
+    }
+}
