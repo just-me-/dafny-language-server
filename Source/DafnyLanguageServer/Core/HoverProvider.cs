ab65d1e1f6b297c35b568a7fa8409172633d1ea0
TomK <tkistler@hsr.ch>
2020-05-20 14:02:48 +0200

    Moved Interfaces to own subfolder

---
+++ Source/DafnyLanguageServer/Core/HoverProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using DafnyLanguageServer.Commons;
+using DafnyLanguageServer.Tools;
+using DafnyLanguageServer.WorkspaceManager;
+using Microsoft.Boogie;
+using Microsoft.Dafny;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol.Server;
+
+namespace DafnyLanguageServer.Core
+{
+    /// <summary>
+    /// This service is used by the <c>TextDocumentSyncTaskHandler</c> to provide verification for Dafny files with <c>SendDiagnostics</c>. 
+    /// </summary>
+    public class HoverProvider : IHoverProvider
+    {
+        
+    }
+}
