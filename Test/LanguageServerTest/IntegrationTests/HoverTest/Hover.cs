635b3d401513fe725daa8d4453e89e8125f50702
TomK <tkistler@hsr.ch>
2020-05-21 15:14:12 +0200

    added simpel hover tests

--- Test/LanguageServerTest/IntegrationTests/HoverTest/Hover.cs
+++ Test/LanguageServerTest/IntegrationTests/HoverTest/Hover.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using NUnit.Framework;
@@ -15,11 +16,39 @@ namespace HoverTest
         }
 
         [Test]
-        public void Test()
+        public void Test1()
         {
             Client.TextDocument.DidOpen(Files.hv_hover, "dfy");
             var result = Client.TextDocument.Hover(Files.hv_hover, 6, 14).Result;
-            var strings = result.Contents.MarkedStrings.ToList();
+            var actual = result.Contents.MarkedStrings.Select(x => x.Value).ToList();
+
+            List<string> expected = new List<string>()
+            {
+                "Symbol: r at Line 7 in hover.dfy",
+                "Kind: Variable",
+                "Type: int",
+                "Scope: addOne",
+                "Declaration: r at Line 5 in hover.dfy"
+            };
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Client.TextDocument.DidOpen(Files.hv_hover, "dfy");
+            var result = Client.TextDocument.Hover(Files.hv_hover, 0, 6).Result;
+            var actual = result.Contents.MarkedStrings.Select(x => x.Value).ToList();
+
+            List<string> expected = new List<string>()
+            {
+                "Symbol: MyClass at Line 1 in hover.dfy",
+                "Kind: Class",
+                "Type: N/A",
+                "Scope: _module",
+                "Declaration: This symbol is a declaration."
+            };
+            CollectionAssert.AreEquivalent(expected, actual);
         }
     }
 }
