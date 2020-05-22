f1351d29e43417cff25521abad3dbf60854a6352
TomK <tkistler@hsr.ch>
2020-05-22 12:44:25 +0200

    Performance test is running

--- Test/LanguageServerTest/IntegrationTests/PerformanceTest/LargeFileEdits.cs
+++ Test/LanguageServerTest/IntegrationTests/PerformanceTest/LargeFileEdits.cs
@@ -1,7 +1,7 @@
-using System.Diagnostics;
-using System.Threading.Tasks;
+using DafnyLanguageServer.Handler;
 using NUnit.Framework;
-using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using System;
+using System.Diagnostics;
 using TestCommons;
 using Files = TestCommons.Paths;
 
@@ -20,36 +20,20 @@ namespace PerformanceTest
             var stopwatch = new Stopwatch();
             stopwatch.Start();
             Client.TextDocument.DidOpen(Files.pf_large, "dfy");
-            
-            var contentChanges = new TextDocumentContentChangeEvent()
-            {
-                Range = new Range
-                {
-                    Start = new Position
-                    {
-                        Line = 6600,
-                        Character = 100
-                    },
-                    End = new Position
-                    {
-                        Line = 6600,
-                        Character = 100
-                    }
-                },
-                RangeLength = 0,
-                Text = " "
-            };
-            for (int i = 0; i < 10; i++)
-            {
-                Client.SendNotification("textDocument/didChange", contentChanges);
-            }
 
-            var foo = Client.TextDocument.Hover(Files.pf_large, 6600, 100);
-            //Client.IsReady.Wait();
+
+            CompilerParams p = new CompilerParams
+            {
+                CompilationArguments = new string[] { },
+                FileToCompile = Files.pf_large
+            };
+            Client.SendRequest<CompilerResults>("compile", p, CancellationSource.Token).Wait();
+
 
             stopwatch.Stop();
             var elapsed_time = stopwatch.ElapsedMilliseconds;
-            //Assert.Less(elapsed_time, 10e3, "Runtime takes too long!" + elapsed_time);
+            Console.WriteLine($"Performancetest took {elapsed_time}ms");
+            Assert.Less(elapsed_time, 5e3, "Runtime takes too long!" + elapsed_time);
         }
     }
 }
