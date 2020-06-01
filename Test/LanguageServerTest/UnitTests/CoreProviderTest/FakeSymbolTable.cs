9079aca89e50bb936f032581ed3f6dcf5eef2672
TomK <tkistler@hsr.ch>
2020-06-01 21:35:47 +0200

    added rename-provider tests with a fake symbol table manager. added fake symbol table generator.

---
+++ Test/LanguageServerTest/UnitTests/CoreProviderTest/FakeSymbolTable.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DafnyLanguageServer.SymbolTable;
+using Microsoft.Boogie;
+
+namespace CoreProviderTest
+{
+    public class FakeSymbolTable : ISymbolTableGenerator
+    {
+        /// <summary>
+        /// Builds a short fake symbol table which can be used for testing.
+        /// </summary>
+        /// <returns>root symbol of the fake table</returns>
+        /// <remarks> 
+        ///                     root
+        ///           another          decl
+        ///                         use1   use2
+        /// </remarks>
+        public ISymbol GenerateSymbolTable()
+        {
+            ISymbol root = SymbolTableGenerator.GetEmptySymbolTable();
+            SymbolInformation decl = new SymbolInformation
+            {
+                Position = new TokenPosition() { Token = new Token(1, 11) },
+                Name = "barapapa",
+                Parent = root
+
+            };
+            decl.DeclarationOrigin = decl;
+
+            SymbolInformation use1 = new SymbolInformation
+            {
+                Position = new TokenPosition() { Token = new Token(2, 22) },
+                Name = "barapapa",
+                Parent = root,
+                DeclarationOrigin = decl
+            };
+
+            SymbolInformation use2 = new SymbolInformation
+            {
+                Position = new TokenPosition() { Token = new Token(3, 33) },
+                Name = "barapapa",
+                Parent = root,
+                DeclarationOrigin = decl
+
+            };
+
+            SymbolInformation anotherSymbol = new SymbolInformation
+            {
+                Position = new TokenPosition() { Token = new Token(4, 443) },
+                Name = "bubu",
+                Parent = root,
+            };
+            anotherSymbol.DeclarationOrigin = anotherSymbol;
+
+            root.ChildrenHash = new Dictionary<string, ISymbol>();
+            root.ChildrenHash.Add("barapapa", decl);
+            root.ChildrenHash.Add("bubu", anotherSymbol);
+            decl.Usages = new List<ISymbol> { use1, use2 };
+
+            return root;
+        }
+    }
+}
---
+++ Test/LanguageServerTest/UnitTests/CoreProviderTest/RenameProviderTests.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DafnyLanguageServer.Core;
+using DafnyLanguageServer.SymbolTable;
+using Microsoft.Boogie;
+using NUnit.Framework;
+
+namespace CoreProviderTest
+{
+    public class SymbolManagerFakeForRename : ISymbolTableManager
+    {
+
+        public ISymbol GetSymbolByPosition(Uri file, int line, int character)
+        {
+            ISymbol root = new FakeSymbolTable().GenerateSymbolTable();
+            return root["barapapa"].Usages[1];
+
+        }
+
+        public ISymbol GetSymbolWrapperForCurrentScope(Uri file, int line, int character)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISymbol GetClosestSymbolByName(ISymbol entryPoint, string symbolName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ISymbol> GetAllDeclarationForSymbolInScope(ISymbol symbol, Predicate<ISymbol> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISymbol GetOriginFromSymbol(ISymbol symbol)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISymbol GetClassOriginFromSymbol(ISymbol symbol)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ISymbol> GetAllSymbolDeclarations()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+    [TestFixture]
+    [Category("Unit")]
+    public class RenameProviderTests
+    {
+        [Test]
+        public void TestAssembling()
+        {
+            ISymbolTableManager manager = new SymbolManagerFakeForRename();
+            var provider = new RenameProvider(manager);
+            var result = provider.GetRenameChanges("oink", new Uri("file:///N:/u/l.l"), 2, 22);
+
+            Assert.AreEqual("file:///N:/u/l.l", result.Changes.Keys.First().ToString(),
+                "The fake instance is not assigning URIs. It should be the default-null uri.");
+
+            var changes = result.Changes.Values.First();
+            var aslist = changes.ToList();
+            aslist.Sort(
+                (te1, te2) => (int) te1.Range.Start.Line - (int) te2.Range.Start.Line
+            );
+            foreach (var c in aslist)
+            {
+                Assert.AreEqual("oink", c.NewText, "New Text was not as expected");
+                Assert.AreEqual(8, c.Range.End.Character - c.Range.Start.Character, "The Range was not as expected");
+                Assert.AreEqual(0, c.Range.End.Line - c.Range.Start.Line, "Edit is not on the same line");
+            }
+
+            Assert.AreEqual(0, aslist[0].Range.Start.Line, "Edit 0 starts at wrong line");
+            Assert.AreEqual(10, aslist[0].Range.Start.Character, "Edit 0 starts at wrong char");
+
+            Assert.AreEqual(1, aslist[1].Range.Start.Line, "Edit 1 starts at wrong line");
+            Assert.AreEqual(21, aslist[1].Range.Start.Character, "Edit 1 starts at wrong char");
+
+            Assert.AreEqual(2, aslist[2].Range.Start.Line, "Edit 2 starts at wrong line");
+            Assert.AreEqual(32, aslist[2].Range.Start.Character, "Edit 2 starts at wrong char");
+        }
+
+        [Test]
+        public void TestOutcome()
+        {
+            ISymbolTableManager manager = new SymbolManagerFakeForRename();
+            var provider = new RenameProvider(manager);
+            var result = provider.GetRenameChanges("oink", new Uri("file:///N:/u/l.l"), 2, 22);
+
+            Assert.IsFalse(provider.Outcome.Error, "no error expected in rename-outcome");
+        }
+
+        [Test]
+        public void IrregularName()
+        {
+            ISymbolTableManager manager = new SymbolManagerFakeForRename();
+            var provider = new RenameProvider(manager);
+            var result = provider.GetRenameChanges("{{}}}", new Uri("file:///N:/u/l.l"), 2, 22);
+
+            Assert.IsTrue(provider.Outcome.Error, "error expected in rename-outcome");
+            Assert.AreEqual(provider.Outcome.Msg,
+                "{{}}}" + DafnyLanguageServer.Resources.LoggingMessages.rename_only_digits);
+        }
+
+
+        [Test]
+        public void UnderScoreName()
+        {
+            ISymbolTableManager manager = new SymbolManagerFakeForRename();
+            var provider = new RenameProvider(manager);
+            var result = provider.GetRenameChanges("_abc", new Uri("file:///N:/u/l.l"), 2, 22);
+
+            Assert.IsTrue(provider.Outcome.Error, "error expected in rename-outcome");
+            Assert.AreEqual(provider.Outcome.Msg,
+                DafnyLanguageServer.Resources.LoggingMessages.rename_start_with_underscore);
+        }
+
+        [Test]
+        public void ReservedWord()
+        {
+            ISymbolTableManager manager = new SymbolManagerFakeForRename();
+            var provider = new RenameProvider(manager);
+            var result = provider.GetRenameChanges("method", new Uri("file:///N:/u/l.l"), 2, 22);
+
+            Assert.IsTrue(provider.Outcome.Error, "error expected in rename-outcome");
+            Assert.AreEqual(provider.Outcome.Msg,
+                "method" + DafnyLanguageServer.Resources.LoggingMessages.rename_reserved_word);
+        }
+    }
+}
